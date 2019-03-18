201007f96509e58f049b457f784e677f7f1ab7f6
goodz2k17 <goodz2k17@gmail.com>
2019-03-18 15:18:45 +0530

    asfd

--- api/models/ESp8266ONE.js
+++ api/models/ESp8266ONE.js
@@ -5,7 +5,7 @@ const esp8266oneSchema = mongoose.Schema({
     moduleID: { type: String, required: true },
     mappedTo: { type: String, required: true},
     value: { type: String, required: true},
-    datetime: { type: Number, required: true}
+    datetime: { type: String, required: true}
 });
 
 module.exports = mongoose.model('esp8266one', esp8266oneSchema);
